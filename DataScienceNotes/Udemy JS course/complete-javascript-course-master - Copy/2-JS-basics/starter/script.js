ace552ac7cf09aa7cf2288ac0ebea056ae8641bd
Javier Benitez <javier.c.benitez@gmail.com>
2019-03-25 18:28:52 -0400

    sync

---
+++ DataScienceNotes/Udemy JS course/complete-javascript-course-master - Copy/2-JS-basics/starter/script.js
@@ -0,0 +1,40 @@
+// var firstname = 'Javier';
+// console.log(firstname);
+
+// var lastname = 'Benitez';
+// var age = 28;
+
+// var fullage = true;
+// console.log(fullage)
+
+// var job;
+// console.log(job)
+
+// job = 'teacher'
+// console.log(job)
+
+/*
+var firstname = 'mateo'
+var age = 2;
+
+console.log(firstname + ' ' + age  )
+
+var job, isMarried;
+job = 'baby shark';
+isMarried = false;
+
+console.log(firstname + ' is a ' + age + ' year old ' + job + '. Is he married? '  + isMarried);
+
+
+age = 'two';
+job = 'driver';
+
+alert(firstname + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstname + ' ' + lastName);
+*/
+
+//Lecture 10: Basic operators xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
